2e4d18dbf529fe130072475e1d8de8d781275194
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-01-18 19:58:25 +0300

    Passed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+const saturdayFun = function(activity1="roller-skate", activity2="bathe my dog") {
+    if (activity1){
+      return ( `This Saturday, I want to ${activity1}!`)
+    }
+    else if(activity2) {
+      return ( `This Saturday, I want to ${activity2}!`)
+    }
+}
+
+const mondayWork = function(param1 = "go to the office", param2 = "work from home") {
+    if(param1){
+        return(`This Monday, I will ${param1}.`);
+    }
+    else if(param2){
+        return(`This Monday, I will ${param2}.`);
+    }
+}
+
+const wrapAdjective = function(style="*") {
+    return function (adj = "special") {
+        return `You are ${style}${adj}${style}!`
+    }
+}
